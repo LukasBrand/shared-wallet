e8c29277ceb8745054c70232d9915ed9cf418296
Lukas Brand <lukasbrand48@gmail.com>
2022-01-09 23:32:54 +0100

    Removed LiveData from CreateAccount

--- app/src/main/java/com/lukasbrand/sharedwallet/ui/account/create/CreateAccountFragment.kt
+++ app/src/main/java/com/lukasbrand/sharedwallet/ui/account/create/CreateAccountFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.lukasbrand.sharedwallet.R
 import com.lukasbrand.sharedwallet.databinding.CreateAccountFragmentBinding
+import com.lukasbrand.sharedwallet.exhaustive
+import com.lukasbrand.sharedwallet.types.Navigator
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CreateAccountFragment : Fragment() {
@@ -31,10 +39,33 @@ class CreateAccountFragment : Fragment() {
             createAccountViewModel = viewModel
         }
 
-        viewModel.onAccountCreated.observe(viewLifecycleOwner) {
-            this.findNavController().navigate(
-                CreateAccountFragmentDirections.actionCreateAccountFragmentToListExpensesFragment()
-            )
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                //This is not clear to me. If two StateFlows are collected inside a single
+                //coroutine launch scope only the first will be executed. Could be a bug
+                launch {
+                    viewModel.createAccountCompleted.collect { navigator ->
+                        when (navigator) {
+                            is Navigator.Navigate -> {
+                                findNavController().navigate(
+                                    CreateAccountFragmentDirections.actionCreateAccountFragmentToListExpensesFragment()
+                                )
+                                viewModel.onNavigatedAfterAccountCreateCompleted()
+                            }
+                            is Navigator.Error -> {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Could not switch to List Expenses: '${navigator.exception.message}'",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            Navigator.Loading -> {}
+                            Navigator.Stay -> {}
+                        }.exhaustive
+                    }
+                }
+            }
         }
         return binding.root
     }
--- app/src/main/java/com/lukasbrand/sharedwallet/ui/account/create/CreateAccountViewModel.kt
+++ app/src/main/java/com/lukasbrand/sharedwallet/ui/account/create/CreateAccountViewModel.kt
@@ -1,15 +1,16 @@
 package com.lukasbrand.sharedwallet.ui.account.create
 
 import android.graphics.Bitmap
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lukasbrand.sharedwallet.data.User
 import com.lukasbrand.sharedwallet.data.repository.AuthenticationRepository
 import com.lukasbrand.sharedwallet.data.repository.UsersRepository
 import com.lukasbrand.sharedwallet.services.message.MessageSendService
+import com.lukasbrand.sharedwallet.types.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,29 +21,32 @@ class CreateAccountViewModel @Inject constructor(
     private val usersRepository: UsersRepository
 ) : ViewModel() {
 
-    private val _onAccountCreated: MutableLiveData<User> = MutableLiveData()
-    val onAccountCreated: LiveData<User>
-        get() = _onAccountCreated
-
-    val name: MutableLiveData<String> = MutableLiveData("")
-    val email: MutableLiveData<String> = MutableLiveData("")
-    val password: MutableLiveData<String> = MutableLiveData("")
-    val picture: MutableLiveData<Bitmap> = MutableLiveData()
+    val name: MutableStateFlow<String> = MutableStateFlow("")
+    val email: MutableStateFlow<String> = MutableStateFlow("")
+    val password: MutableStateFlow<String> = MutableStateFlow("")
+    val picture: MutableStateFlow<Bitmap?> = MutableStateFlow(null)
 
     fun createAccount() {
         viewModelScope.launch {
-            val userId =
-                authenticationRepository.createAccountWithEmailAndPassword(
-                    email.value!!,
-                    password.value!!
-                )
+            val userId = authenticationRepository.createAccountWithEmailAndPassword(
+                email.value,
+                password.value
+            )
 
             val notificationToken = messageSendService.getToken()
 
-            val user = User(userId, name.value!!, email.value!!, notificationToken, picture.value)
+            val user = User(userId, name.value, email.value, notificationToken, picture.value)
             usersRepository.addUser(user)
-            _onAccountCreated.value = user
+            _createAccountCompleted.value = Navigator.Navigate(Unit)
         }
+    }
+
+    private val _createAccountCompleted: MutableStateFlow<Navigator<Unit>> =
+        MutableStateFlow(Navigator.Stay)
+    val createAccountCompleted: StateFlow<Navigator<Unit>> = _createAccountCompleted
+
+    fun onNavigatedAfterAccountCreateCompleted() {
+        _createAccountCompleted.value = Navigator.Stay
     }
 }
 
